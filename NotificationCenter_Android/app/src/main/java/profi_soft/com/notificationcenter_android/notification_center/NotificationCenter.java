2366351cf141a581641586b699b9ab366bb2da87
Marcin Kielesinski <marcin@kielesinski.com>
2017-07-24 13:02:16 +0200

    fixes

--- NotificationCenter_Android/app/src/main/java/profi_soft/com/notificationcenter_android/notification_center/NotificationCenter.java
+++ NotificationCenter_Android/app/src/main/java/profi_soft/com/notificationcenter_android/notification_center/NotificationCenter.java
@@ -12,7 +12,6 @@ import profi_soft.com.notificationcenter_android.notification_center.helpers.Mut
  */
 
 public class NotificationCenter {
-
     private static final String NOTIFY_T_NAME           = "NOTIFY_T_NAME";
     private static final String NOTIFY_T_TARGET         = "NOTIFY_T_TARGET";
     private static final String NOTIFY_T_HANDLER        = "NOTIFY_T_HANDLER";
@@ -23,10 +22,13 @@ public class NotificationCenter {
     private static final String NOTIFICATION_USER_DATA  = "NOTIFICATION_USER_DATA";
 
     private static boolean isRunned = false;
-    private static List<Object> notificationObjects = new CopyOnWriteArrayList<>();
-    private static List<Object> notificationTargets = new CopyOnWriteArrayList<>();
+    private static List<Object> notificationObjects = null;
+    private static List<Object> notificationTargets = null;
 
     public static void init(){
+
+        notificationObjects = new CopyOnWriteArrayList<>();
+        notificationTargets = new CopyOnWriteArrayList<>();
 
         isRunned = true;
         Thread t = new Thread(new Runnable() {
@@ -34,23 +36,33 @@ public class NotificationCenter {
             public void run() {
                 while (isRunned) {
                     try {
-                        for(Object notification: notificationObjects){
+                        if(notificationObjects != null) {
+                            if(notificationObjects.size() > 0) {
+                                Object notification = notificationObjects.get(0);
 
-                            MutableDictionary entry = (MutableDictionary)notification;
-                            String notificationName = (String)entry.objectForKey(NOTIFICATION_NAME);
+                                MutableDictionary entry = (MutableDictionary) notification;
+                                String notificationName = (String) entry.objectForKey(NOTIFICATION_NAME);
 
-                            for(Object e : notificationTargets){
+                                if (notificationTargets != null) {
+                                    for (Object e : notificationTargets) {
 
-                                MutableDictionary target = (MutableDictionary)e;
-                                if(target.objectForKey(NOTIFY_T_NAME).equals(notificationName)){
+                                        if (!isRunned) {
+                                            break;
+                                        }
 
-                                    NotificationCenterInterface handler = (NotificationCenterInterface)target.objectForKey(NOTIFY_T_HANDLER);
-                                    handler.onReceiveNotification(notificationName,
-                                            entry.objectForKey(NOTIFICATION_OBJECT),
-                                            entry.objectForKey(NOTIFICATION_USER_DATA));
+                                        MutableDictionary target = (MutableDictionary) e;
+                                        if (target.objectForKey(NOTIFY_T_NAME).equals(notificationName)) {
+
+                                            NotificationCenterInterface handler = (NotificationCenterInterface) target.objectForKey(NOTIFY_T_HANDLER);
+                                            handler.onReceiveNotification(notificationName,
+                                                    entry.objectForKey(NOTIFICATION_OBJECT),
+                                                    entry.objectForKey(NOTIFICATION_USER_DATA));
+                                        }
+                                    }
                                 }
+
+                                notificationObjects.remove(0);
                             }
-                            notificationObjects.remove(notification);
                         }
 
                         Thread.sleep(1);
@@ -61,51 +73,79 @@ public class NotificationCenter {
                         }
                     }
                 }
+
+                if(notificationObjects != null) {
+                    notificationObjects.clear();
+                    notificationObjects = null;
+                }
+                if(notificationTargets != null) {
+                    notificationTargets.clear();
+                    notificationTargets = null;
+                }
+                System.gc();
+
             }
         });
         t.start();
     }
 
     public static void exit(){
-
-        notificationObjects.clear();
-        notificationTargets.clear();
-        notificationObjects = null;
-        notificationTargets = null;
         isRunned = false;
-        System.gc();
     }
 
 
     public static void post(String name, Object object, Object userData){
 
-        MutableDictionary notification = new MutableDictionary();
-        notification.setObject(name, NOTIFICATION_NAME);
-        notification.setObject(object, NOTIFICATION_OBJECT);
-        notification.setObject(userData, NOTIFICATION_USER_DATA);
+        if(name != null && notificationObjects != null) {
+            MutableDictionary notification = new MutableDictionary();
+            notification.setObject(name, NOTIFICATION_NAME);
+            notification.setObject(object, NOTIFICATION_OBJECT);
+            notification.setObject(userData, NOTIFICATION_USER_DATA);
 
-        notificationObjects.add(notification);
+            notificationObjects.add(notification);
+        }
     }
 
     public static void addObserver(String name, Object target, NotificationCenterInterface handler){
 
-        MutableDictionary entry = new MutableDictionary();
-        entry.setObject(name, NOTIFY_T_NAME);
-        entry.setObject(target, NOTIFY_T_TARGET);
-        entry.setObject(handler, NOTIFY_T_HANDLER);
-        entry.setObject(target.getClass().getSimpleName(), NOTIFY_T_TARGET_NAME);
+        try {
+            if (notificationTargets != null) {
+                if (name != null && target != null && handler != null) {
 
-        notificationTargets.add(entry);
+                    MutableDictionary entry = new MutableDictionary();
+                    entry.setObject(name, NOTIFY_T_NAME);
+                    entry.setObject(target, NOTIFY_T_TARGET);
+                    entry.setObject(handler, NOTIFY_T_HANDLER);
+                    entry.setObject(target.getClass().getSimpleName(), NOTIFY_T_TARGET_NAME);
+
+                    notificationTargets.add(entry);
+                }
+            }
+        } catch (Exception e){
+            if(Debug.ENABLED){
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void removeObserver(Object target){
-        String targetName = target.getClass().getSimpleName();
+        try {
+            if(target != null) {
+                String targetName = target.getClass().getSimpleName();
 
-        for(Object e : notificationTargets){
+                if (notificationTargets != null) {
+                    for (Object e : notificationTargets) {
 
-            MutableDictionary entry = (MutableDictionary)e;
-            if(entry.objectForKey(NOTIFY_T_TARGET_NAME).equals(targetName)){
-                notificationTargets.remove(e);
+                        MutableDictionary entry = (MutableDictionary) e;
+                        if (entry.objectForKey(NOTIFY_T_TARGET_NAME).equals(targetName)) {
+                            notificationTargets.remove(e);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e){
+            if(Debug.ENABLED){
+                e.printStackTrace();
             }
         }
     }
